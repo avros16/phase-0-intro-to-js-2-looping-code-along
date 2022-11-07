dd3a733ef400bc0ed9b0feeb19e2036866f3d169
Ana Rosario <arosario.mrd@gmail.com>
2022-11-06 21:57:07 -0500

    commit tree be7fa5b4e6a5fc69fdc33ab9b6ae9e0acd912ad7

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+
+function writeCards (name, event) {
+    let cards = [];
+    for (let i = 0; i <name.length; i++) {
+       cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+        debugger;
+        
+}
+return cards;
+}
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
